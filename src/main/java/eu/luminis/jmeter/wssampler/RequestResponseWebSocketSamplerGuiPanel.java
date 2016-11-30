19286dfc88444daf98325fd4b59f729d2f20f81c
Peter Doornbosch <peter.doornbosch@luminis.eu>
2016-11-30 21:54:34 +0100

    refactor (structure ui components)

--- src/main/java/eu/luminis/jmeter/wssampler/RequestResponseWebSocketSamplerGuiPanel.java
+++ src/main/java/eu/luminis/jmeter/wssampler/RequestResponseWebSocketSamplerGuiPanel.java
@@ -65,137 +65,151 @@ public class RequestResponseWebSocketSamplerGuiPanel extends WebSocketSamplerGui
     private void init() {
 
         JPanel boxPanel = new JPanel();
-        boxPanel.setLayout(new BoxLayout(boxPanel, Y_AXIS));
-
-        JPanel connectionPanel = new JPanel();
-        connectionPanel.setLayout(new BoxLayout(connectionPanel, Y_AXIS));
-        connectionPanel.setBorder(BorderFactory.createTitledBorder("Connection"));
-        JPanel innerConnectionButtonPanel = new JPanel();
-        innerConnectionButtonPanel.setLayout(new BoxLayout(innerConnectionButtonPanel, Y_AXIS));
-        reuseConnection = new JRadioButton("use existing connection");
-        reuseConnection.addActionListener(e -> handleConnectionRadio(e));
-        innerConnectionButtonPanel.add(reuseConnection);
-        newConnection = new JRadioButton("setup new connection");
-        newConnection.setSelected(true);
-        newConnection.addActionListener(e -> handleConnectionRadio(e));
-        innerConnectionButtonPanel.add(newConnection);
-        ButtonGroup connectionButtons = new ButtonGroup();
-        connectionButtons.add(newConnection);
-        connectionButtons.add(reuseConnection);
-        JPanel outerConnectionButtonPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        outerConnectionButtonPanel.add(innerConnectionButtonPanel);
-        connectionPanel.add(outerConnectionButtonPanel);
-        boxPanel.add(connectionPanel);
-        JPanel urlPanel = new JPanel();
-        urlPanel.setLayout(new BoxLayout(urlPanel, X_AXIS));
-        connectionPanel.add(urlPanel);
-        urlPanel.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(5,0,0,0),
-                BorderFactory.createCompoundBorder(BorderFactory.createTitledBorder("Server URL"), BorderFactory.createEmptyBorder(3,5,5,0))));
-        JLabel serverLabel = new JLabel("Server name or IP:");
-        urlPanel.add(serverLabel);
-        connectionRelatedSettings.add(serverLabel);
-        connectionRelatedSettings.add(urlPanel);
-        serverField = new JTextField();
-        serverField.setColumns(20);
-        urlPanel.add(serverField);
-        JLabel portLabel = new JLabel("Port:");
-        urlPanel.add(portLabel);
-        connectionRelatedSettings.add(portLabel);
-        connectionRelatedSettings.add(serverField);
-        portField = new JTextField();
-        addIntegerRangeCheck(portField, 1, 65535);
-        portField.setColumns(5);
-        portField.setMaximumSize(portField.getPreferredSize());
-        connectionRelatedSettings.add(portField);
-        urlPanel.add(portField);
-        JLabel pathLabel = new JLabel("Path:");
-        urlPanel.add(pathLabel);
-        connectionRelatedSettings.add(pathLabel);
-        pathField = new JTextField();
-        pathField.setColumns(20);
-        urlPanel.add(pathField);
-        connectionRelatedSettings.add(pathField);
-        JPanel connectionTimeoutPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        connectionTimeoutPanel.setBorder(BorderFactory.createEmptyBorder(1, 4, 1, 0));
         {
-            JLabel connectionTimeoutLabel = new JLabel("Connection timeout (ms):");
-            connectionTimeoutPanel.add(connectionTimeoutLabel);
-            connectionTimeoutField = new JTextField();
-            connectionTimeoutField.setColumns(10);
-            connectionTimeoutPanel.add(connectionTimeoutField);
-            JLabel connectionTimeoutErrorLabel = new JLabel();
-            connectionTimeoutErrorLabel.setForeground(Color.RED);
-            addIntegerRangeCheck(connectionTimeoutField, MIN_CONNECTION_TIMEOUT, MAX_CONNECTION_TIMEOUT, connectionTimeoutErrorLabel);
-            connectionTimeoutPanel.add(connectionTimeoutErrorLabel);
-            connectionRelatedSettings.add(connectionTimeoutLabel);
-            connectionRelatedSettings.add(connectionTimeoutField);
+            boxPanel.setLayout(new BoxLayout(boxPanel, Y_AXIS));
+
+            JPanel connectionPanel = new JPanel();
+            {
+                connectionPanel.setLayout(new BoxLayout(connectionPanel, Y_AXIS));
+                connectionPanel.setBorder(BorderFactory.createTitledBorder("Connection"));
+
+                JPanel outerConnectionButtonPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+                {
+                    JPanel innerConnectionButtonPanel = new JPanel();
+                    {
+                        innerConnectionButtonPanel.setLayout(new BoxLayout(innerConnectionButtonPanel, Y_AXIS));
+                        reuseConnection = new JRadioButton("use existing connection");
+                        reuseConnection.addActionListener(e -> handleConnectionRadio(e));
+                        innerConnectionButtonPanel.add(reuseConnection);
+                        newConnection = new JRadioButton("setup new connection");
+                        newConnection.setSelected(true);
+                        newConnection.addActionListener(e -> handleConnectionRadio(e));
+                        innerConnectionButtonPanel.add(newConnection);
+
+                        ButtonGroup connectionButtons = new ButtonGroup();
+                        connectionButtons.add(newConnection);
+                        connectionButtons.add(reuseConnection);
+                    }
+                    outerConnectionButtonPanel.add(innerConnectionButtonPanel);
+                }
+                connectionPanel.add(outerConnectionButtonPanel);
+
+                JPanel urlPanel = new JPanel();
+                {
+                    urlPanel.setLayout(new BoxLayout(urlPanel, X_AXIS));
+                    urlPanel.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(5, 0, 0, 0),
+                            BorderFactory.createCompoundBorder(BorderFactory.createTitledBorder("Server URL"), BorderFactory.createEmptyBorder(3, 5, 5, 0))));
+                    JLabel serverLabel = new JLabel("Server name or IP:");
+                    urlPanel.add(serverLabel);
+                    connectionRelatedSettings.add(serverLabel);
+                    connectionRelatedSettings.add(urlPanel);
+                    serverField = new JTextField();
+                    serverField.setColumns(20);
+                    urlPanel.add(serverField);
+                    JLabel portLabel = new JLabel("Port:");
+                    urlPanel.add(portLabel);
+                    connectionRelatedSettings.add(portLabel);
+                    connectionRelatedSettings.add(serverField);
+                    portField = new JTextField();
+                    addIntegerRangeCheck(portField, 1, 65535);
+                    portField.setColumns(5);
+                    portField.setMaximumSize(portField.getPreferredSize());
+                    connectionRelatedSettings.add(portField);
+                    urlPanel.add(portField);
+                    JLabel pathLabel = new JLabel("Path:");
+                    urlPanel.add(pathLabel);
+                    connectionRelatedSettings.add(pathLabel);
+                    pathField = new JTextField();
+                    pathField.setColumns(20);
+                    urlPanel.add(pathField);
+                    connectionRelatedSettings.add(pathField);
+                }
+                connectionPanel.add(urlPanel);
+
+                JPanel connectionTimeoutPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+                {
+                    connectionTimeoutPanel.setBorder(BorderFactory.createEmptyBorder(1, 4, 1, 0));
+                    JLabel connectionTimeoutLabel = new JLabel("Connection timeout (ms):");
+                    connectionTimeoutPanel.add(connectionTimeoutLabel);
+                    connectionTimeoutField = new JTextField();
+                    connectionTimeoutField.setColumns(10);
+                    connectionTimeoutPanel.add(connectionTimeoutField);
+                    JLabel connectionTimeoutErrorLabel = new JLabel();
+                    connectionTimeoutErrorLabel.setForeground(Color.RED);
+                    addIntegerRangeCheck(connectionTimeoutField, MIN_CONNECTION_TIMEOUT, MAX_CONNECTION_TIMEOUT, connectionTimeoutErrorLabel);
+                    connectionTimeoutPanel.add(connectionTimeoutErrorLabel);
+                    connectionRelatedSettings.add(connectionTimeoutLabel);
+                    connectionRelatedSettings.add(connectionTimeoutField);
+                }
+                connectionPanel.add(connectionTimeoutPanel);
+            }
+            boxPanel.add(connectionPanel);
+
+            JPanel dataPanel = new JPanel();
+            {
+                dataPanel.setBorder(BorderFactory.createTitledBorder("Data"));
+                dataPanel.setLayout(new BoxLayout(dataPanel, BoxLayout.Y_AXIS));
+
+                JPanel topBar = new JPanel();
+                {
+                    topBar.setLayout(new BoxLayout(topBar, BoxLayout.X_AXIS));
+                    String[] typeOptions = {TEXT, BINARY};
+                    typeSelector = new JComboBox(typeOptions);
+                    typeSelector.setMaximumSize(typeSelector.getMinimumSize());
+                    typeSelector.addActionListener(e -> {
+                        checkBinary();
+                    });
+                    topBar.add(typeSelector);
+                    topBar.add(Box.createHorizontalStrut(10));
+                    messageField = new JLabel();
+                    messageField.setBackground(Color.YELLOW);
+                    messageField.setForeground(Color.RED);
+                    topBar.add(messageField);
+                    topBar.add(Box.createHorizontalGlue());
+                }
+                dataPanel.add(topBar);
+
+                JPanel dataZone = new JPanel();
+                {
+                    dataZone.setLayout(new BoxLayout(dataZone, X_AXIS));
+                    dataZone.add(Box.createHorizontalStrut(5));
+                    dataZone.add(new JLabel("Request data: "));
+                    requestDataField = new JTextField();
+                    requestDataField.setColumns(40);
+                    // Add a simple (huhuh!) on-change handler....
+                    requestDataField.getDocument().addDocumentListener(new DocumentListener() {
+                        public void changedUpdate(DocumentEvent e) {
+                            checkBinary();
+                        }
+
+                        public void removeUpdate(DocumentEvent e) {
+                            checkBinary();
+                        }
+
+                        public void insertUpdate(DocumentEvent e) {
+                            checkBinary();
+                        }
+                    });
+                    dataZone.add(requestDataField);
+                }
+                dataPanel.add(dataZone);
+
+                JPanel requestSettingsPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+                {
+                    requestSettingsPanel.add(new JLabel("Response (read) timeout (ms): "));
+                    readTimeoutField = new JTextField();
+                    readTimeoutField.setColumns(10);
+                    JLabel readTimeoutErrorField = new JLabel();
+                    readTimeoutErrorField.setForeground(Color.RED);
+                    addIntegerRangeCheck(readTimeoutField, MIN_READ_TIMEOUT, MAX_READ_TIMEOUT, readTimeoutErrorField);
+                    requestSettingsPanel.add(readTimeoutField);
+                    requestSettingsPanel.add(readTimeoutErrorField);
+                }
+                dataPanel.add(requestSettingsPanel);
+            }
+            boxPanel.add(dataPanel);
+
         }
-        connectionPanel.add(connectionTimeoutPanel);
-
-        JPanel dataPanel = new JPanel();
-        {
-            dataPanel.setBorder(BorderFactory.createTitledBorder("Data"));
-            dataPanel.setLayout(new BoxLayout(dataPanel, BoxLayout.Y_AXIS));
-
-            JPanel topBar = new JPanel();
-            {
-                topBar.setLayout(new BoxLayout(topBar, BoxLayout.X_AXIS));
-                String[] typeOptions = {TEXT, BINARY};
-                typeSelector = new JComboBox(typeOptions);
-                typeSelector.setMaximumSize(typeSelector.getMinimumSize());
-                typeSelector.addActionListener(e -> {
-                    checkBinary();
-                });
-                topBar.add(typeSelector);
-                topBar.add(Box.createHorizontalStrut(10));
-                messageField = new JLabel();
-                messageField.setBackground(Color.YELLOW);
-                messageField.setForeground(Color.RED);
-                topBar.add(messageField);
-                topBar.add(Box.createHorizontalGlue());
-            }
-            dataPanel.add(topBar);
-
-            JPanel dataZone = new JPanel();
-            {
-                dataZone.setLayout(new BoxLayout(dataZone, X_AXIS));
-                dataZone.add(Box.createHorizontalStrut(5));
-                dataZone.add(new JLabel("Request data: "));
-                requestDataField = new JTextField();
-                requestDataField.setColumns(40);
-                // Add a simple (huhuh!) on-change handler....
-                requestDataField.getDocument().addDocumentListener(new DocumentListener() {
-                    public void changedUpdate(DocumentEvent e) {
-                        checkBinary();
-                    }
-
-                    public void removeUpdate(DocumentEvent e) {
-                        checkBinary();
-                    }
-
-                    public void insertUpdate(DocumentEvent e) {
-                        checkBinary();
-                    }
-                });
-                dataZone.add(requestDataField);
-            }
-            dataPanel.add(dataZone);
-
-            JPanel requestSettingsPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-            {
-                requestSettingsPanel.add(new JLabel("Response (read) timeout (ms): "));
-                readTimeoutField = new JTextField();
-                readTimeoutField.setColumns(10);
-                JLabel readTimeoutErrorField = new JLabel();
-                readTimeoutErrorField.setForeground(Color.RED);
-                addIntegerRangeCheck(readTimeoutField, MIN_READ_TIMEOUT, MAX_READ_TIMEOUT, readTimeoutErrorField);
-                requestSettingsPanel.add(readTimeoutField);
-                requestSettingsPanel.add(readTimeoutErrorField);
-            }
-            dataPanel.add(requestSettingsPanel);
-        }
-        boxPanel.add(dataPanel);
-
         setLayout(new BorderLayout());
         add(boxPanel, BorderLayout.NORTH);
     }
