232ed5f4047668d485c238385edb1f53a7c98480
Peter Doornbosch <peter.doornbosch@luminis.eu>
2017-01-01 18:45:41 +0100

    updated copyright

--- src/test/java/eu/luminis/websocket/WebSocketClientTest.java
+++ src/test/java/eu/luminis/websocket/WebSocketClientTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Peter Doornbosch
+ * Copyright 2016, 2017 Peter Doornbosch
  *
  * This file is part of JMeter-WebSocket-Samplers, a JMeter add-on for load-testing WebSocket applications.
  *
